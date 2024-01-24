8c84db00d70893c49a35526124df40ea1c08b17e
vision1v1 <vision1v1@163.com>
2024-01-24 19:24:26 +0800

    调试代码

--- debug_albert/debug_model.py
+++ debug_albert/debug_model.py
@@ -1,6 +1,13 @@
 import os
+from transformers import AlbertConfig, AlbertTokenizer, AlbertModel, AlbertForMaskedLM
 
 albert_xxlarge_v2_path = os.path.join(os.getenv('my_data_dir'), "pretrained", "albert", "albert-xxlarge-v2")
+
+
+def debug_tokenizer():
+    print(albert_xxlarge_v2_path)
+    tokenizer = AlbertTokenizer.from_pretrained(albert_xxlarge_v2_path)
+    ...
 
 def debug_fill_mask():
     """
@@ -15,5 +22,6 @@ def debug_fill_mask():
     ...
 
 if __name__ == "__main__":
-    debug_fill_mask()
+    debug_tokenizer()
+    # debug_fill_mask()
     ...
