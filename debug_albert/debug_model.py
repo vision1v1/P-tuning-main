aeb98a9df44fccbd187cd80a9accff1e529f1c2d
vision1v1 <vision1v1@163.com>
2024-01-26 18:53:49 +0800

    学习

--- debug_albert/debug_model.py
+++ debug_albert/debug_model.py
@@ -5,9 +5,58 @@ albert_xxlarge_v2_path = os.path.join(os.getenv('my_data_dir'), "pretrained", "a
 
 
 def debug_tokenizer():
-    print(albert_xxlarge_v2_path)
-    tokenizer = AlbertTokenizer.from_pretrained(albert_xxlarge_v2_path)
+    """
+    分词
+    """
+    tokenizer:AlbertTokenizer = AlbertTokenizer.from_pretrained(albert_xxlarge_v2_path)
+    text = "Replace me by any text you'd like."
+    tokens = tokenizer.tokenize(text)
+    print("tokenized tokens :", tokens, sep='\n', end='\n\n')
+
+    # 方式1
+    token_ids = tokenizer.encode(tokens, add_special_tokens=False)
+    print("token_ids :", token_ids, sep='\n', end='\n\n')
+
+    # 方式2
+    token_ids = tokenizer.convert_tokens_to_ids(tokens)
+    print("token_ids :", token_ids, sep='\n', end='\n\n')
+
+    
+
+
+def debug_special_token():
+    """
+    调试 特殊 token
+    """
+
+    tokenizer:AlbertTokenizer = AlbertTokenizer.from_pretrained(albert_xxlarge_v2_path)
+    text = "Replace me by any text you'd like."
+    tokens = tokenizer.tokenize(text)
+
+    token_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+    # 返回一个加好特殊token的token序列。
+    token_ids = tokenizer.build_inputs_with_special_tokens(token_ids_0=token_ids)
+    print("token_ids :", token_ids, sep='\n', end='\n\n')
+
+    # 获取特殊token数量，pair=False 表示单个句子
+    num_special = tokenizer.num_special_tokens_to_add(pair=False)
+    print("num_special :", num_special, sep='\n', end='\n\n')
+
+
     ...
+
+def debug_model_inputs():
+    """
+    模型输入
+    """
+    tokenizer:AlbertTokenizer = AlbertTokenizer.from_pretrained(albert_xxlarge_v2_path)
+    text = "Replace me by any text you'd like."
+    model_inputs = tokenizer(text, return_tensors='pt')
+
+    print("model_inputs :", end='\n\n')
+    for k, v in model_inputs.items():
+        print(k, v, sep='\n', end='\n\n')
 
 def debug_fill_mask():
     """
@@ -24,4 +73,5 @@ def debug_fill_mask():
 if __name__ == "__main__":
     debug_tokenizer()
     # debug_fill_mask()
+    # debug_model_inputs()
     ...
---
+++ debug_pt_fewshot/debug_train.py
@@ -0,0 +1,46 @@
+import os
+from transformers import InputExample
+from transformers import AlbertTokenizer
+
+albert_xxlarge_v2_path = os.path.join(os.getenv('my_data_dir'), "pretrained", "albert", "albert-xxlarge-v2")
+tokenizer:AlbertTokenizer = AlbertTokenizer.from_pretrained(albert_xxlarge_v2_path)
+
+def rte_data():
+    """
+    rte 任务描述：自然语言推断任务
+    给模型一个前提（premise），然后问模型一个假设（hypothesis）是否成立。label 成立（entailment）不成立（not_entailment）
+    """
+    
+    # \u2014 为中文横线—
+    data = {
+        "premise": "Even the most draconian proposal \u2014 to reinstate flight limits to ease the bottlenecks at Kennedy \u2014 might backfire, industry analysts say, because airlines would end up shifting flights to Newark Airport, which is already strained. \"Kennedy is the perfect example of putting 10 pounds in a 5-pound bag,\" said Darryl Jenkins, a longtime airline consultant. \"J.F.K. was never set up to be a hub for anybody; its been a gateway,\" he said. But in recent years it became a hub for JetBlue and Delta. These days, delays at Kennedy are so bad, he said, that \"it's backing up the whole country.\".",
+        "hypothesis": "JFK airport is in New York.",
+        "label": "not_entailment",
+        "idx": 2363
+    }
+    return data
+
+
+def data_process():
+    """
+    
+    """
+    data = rte_data()
+
+    examples = []
+    example = InputExample(guid=f'train-{data["idx"]}', text_a=data["premise"], text_b=data["hypothesis"], label=data["label"])
+    
+    print(example.to_json_string())
+    # True:表示上下文，非模板中的提示词。False:表示模板中提示词。
+    a_txt = [(example.text_a, True), ('Question:', False), (example.text_b, True), ("?", False), ("the", False), ("Answer:", False), ("[MASK]", False), (".", False)]
+
+    a_token_ids= [(tokenizer.encode(x, add_special_tokens=False), s) for x, s in a_txt if x] # 如果 x 不是空串
+
+        
+    tokens_a = [token_id for part, _ in a_token_ids for token_id in part]
+    ...
+
+
+if __name__ == "__main__":
+    data_process()
+    ...
